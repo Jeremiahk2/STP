8fecc7e7228a4b0516ce518973a09f98d061be42
Ryan McPhee <rcmcphee@ncsu.edu>
2022-10-31 19:38:54 -0400

    [Class Skeleton] All required classes and interfaces from the UML diagram

---
+++ Project 2/src/edu/ncsu/csc216/stp/model/util/ILog.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.stp.model.util;
+
+/**
+ * @author mcphe
+ *
+ */
+public interface ILog<E> {
+
+}
---
+++ Project 2/src/edu/ncsu/csc216/stp/model/util/ListNode.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.stp.model.util;
+
+/**
+ * @author mcphe
+ *
+ */
+public class ListNode {
+
+}
