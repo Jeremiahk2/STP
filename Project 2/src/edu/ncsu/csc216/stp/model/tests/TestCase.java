eed6d3fe18236fecee6a36ba4764325f368de763
Ryan McPhee <rcmcphee@ncsu.edu>
2022-10-31 21:34:39 -0400

    [Skeleton] Tests package skeleton besides comments complete

--- Project 2/src/edu/ncsu/csc216/stp/model/tests/TestCase.java
+++ Project 2/src/edu/ncsu/csc216/stp/model/tests/TestCase.java
@@ -4,9 +4,86 @@
 package edu.ncsu.csc216.stp.model.tests;
 
 /**
- * @author mcphe
+ * Class that contains the information about each individual test case including the testCaseId,
+ * testType, testDescription, expectedResults, and Ilog of TestResults
+ * @author Ryan McPhee
  *
  */
 public class TestCase {
 
+	
+	/** testCaseId for the TestCase */
+	private String testCaseId;
+	/** testType for the TestCase */
+	private String testType;
+	/** testDescription for the TestCase */
+	private String testDescription;
+	/** exceptedResults for the TestCase */
+	private String expectedResults;
+	/** testPlan for the TestCase */
+	private TestPlan testPlan;
+	
+	public TestCase(String testCaseId, String testType, String testDescription, String expectedResults) {
+		
+	}
+	
+	public String getTestCaseId() {
+		return null;
+	}
+	
+	private void setTestCaseId(String testCaseId) {
+		
+	}
+	
+	public String getTestType() {
+		return null;
+	}
+	
+	private void setTestType(String testType) {
+		
+	}
+	
+	public String getTestDescription() {
+		return null;
+	}
+	
+	private void setTestDescription(String testDescription) {
+		
+	}
+	
+	public String getExcpetedResults() {
+		return null;
+	}
+	
+ 	private void setExpectedResults(String expectedResults) {
+ 		
+ 	}
+ 	
+ 	public void addTestResult(boolean passing, String actualResults) {
+ 		
+ 	}
+ 	
+ 	public String getStatus() {
+ 		return null;
+ 	}
+ 	
+ 	public String getActualResultsLog() {
+ 		return null;
+ 	}
+ 	
+ 	public void setTestPlan(TestPlan testPlan) {
+ 		
+ 	}
+ 	
+ 	public TestPlan getTestPlan() {
+ 		return null;
+ 	}
+ 	
+ 	public String toString() {
+ 		return null;
+ 	}
+ 	
+ 	
+ 	
+	
 }
--- Project 2/src/edu/ncsu/csc216/stp/model/tests/TestResult.java
+++ Project 2/src/edu/ncsu/csc216/stp/model/tests/TestResult.java
@@ -4,9 +4,35 @@
 package edu.ncsu.csc216.stp.model.tests;
 
 /**
- * @author mcphe
+ * Class that contains the information about the results of a single execution of a TestCase. Knows if is
+ * passing or failing and has details about the actual results from execution.
+ * @author Ryan McPhee
  *
  */
 public class TestResult {
 
+	public TestResult(boolean passing, String actualResults) {
+		
+	}
+	
+	private void setActualResults(String actualResults) {
+	
+	}
+
+	public String getActualResults() {
+		return null;
+	}
+	
+	private void setPassing(boolean passing) {
+		
+	}
+	
+	public boolean isPassing() {
+		return false;
+	}
+	
+	public String toString() {
+		return null;
+	}
+	
 }
