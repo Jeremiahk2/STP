9e44bbc55e6e5665977a60a2bd3533aa43bb410d
Ryan McPhee <rcmcphee@ncsu.edu>
2022-11-03 16:54:01 -0400

    [Skeleton] Implement method skeleton within the TestPlanManager class

--- Project 2/src/edu/ncsu/csc216/stp/model/manager/TestPlanManager.java
+++ Project 2/src/edu/ncsu/csc216/stp/model/manager/TestPlanManager.java
@@ -3,10 +3,71 @@
  */
 package edu.ncsu.csc216.stp.model.manager;
 
+import edu.ncsu.csc216.stp.model.test_plans.AbstractTestPlan;
+import edu.ncsu.csc216.stp.model.tests.TestCase;
+
 /**
  * @author Ryan McPhee
  *
  */
 public class TestPlanManager {
 
+	private boolean isChanged;
+	
+	public TestPlanManager() {
+		
+	}
+	
+	public void loadTestPlans(String fileName) {
+		
+	}
+	
+	public void saveTestPlans(String fileName) {
+		
+	}
+	
+	public boolean isChanged() {
+		return false;
+	}
+	
+	public void addTestPlan(String testPlanName) {
+		
+	}
+	
+	public String[] getTestPlanNames() {
+		return null;
+	}
+	
+	private void getFailingTests() {
+		
+	}
+	
+	public void setCurrentTestPlan(String testPlanName) {
+		
+	}
+	
+	public AbstractTestPlan getCurrentTestPlan() {
+		return null;
+	}
+	
+	public void editTestPlan(String testPlanName) {
+		
+	}
+	
+	public void removeTestPlan() {
+		
+	}
+	
+	public void addTestCase(TestCase t) {
+		
+	}
+	
+	public void addTestResult(int idx, boolean passing, String actualResult) {
+		
+	}
+	
+	public void clearTestPlans() {
+		
+	}
+	
 }
