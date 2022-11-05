3534fc4d174b2fb2a3aa517a67845a0e13951659
Jeremiah Knizley <jdknizle@ncsu.edu>
2022-11-04 22:54:47 -0400

    Added class comments for TestPlanWriter. Added WriteTestPlanToFile
    Added method comments for writeTestPlanToFile

--- Project 2/src/edu/ncsu/csc216/stp/model/io/TestPlanWriter.java
+++ Project 2/src/edu/ncsu/csc216/stp/model/io/TestPlanWriter.java
@@ -8,11 +8,23 @@ import edu.ncsu.csc216.stp.model.test_plans.TestPlan;
 import edu.ncsu.csc216.stp.model.util.ISortedList;
 
 /**
+ * Contains static method for writing TestPlans to files.
+ * The format for the file will look like this:
+ * ! (Test plan name)
+ * # (Test case name), (Test case type)
+ * * (Test Description)     //may contain multiple lines, if so they will be numbered: 1. (Test description line 1)\n 2. (Test description line 2)
+ * * (Expected results)
+ * This continues for each case, and subsequently, each plan.
  * @author Jeremiah Knizley
  *
  */
 public class TestPlanWriter {
 
+	/**
+	 * Writes a list of plans to a File in the format described in the class description.
+	 * @param file a File object that is the File to write to.
+	 * @param plans the list of plans to be written in the file
+	 */
 	public static void writeTestPlanFile(File file, ISortedList<TestPlan> plans) {
 		
 	}
