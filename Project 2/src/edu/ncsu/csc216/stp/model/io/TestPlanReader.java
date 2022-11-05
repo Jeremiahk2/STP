2500b9ea49f2df523491e017fe99b2de73406e23
Jeremiah Knizley <jdknizle@ncsu.edu>
2022-11-04 23:01:55 -0400

    Added skeleton for methods in TestPlanReader

--- Project 2/src/edu/ncsu/csc216/stp/model/io/TestPlanReader.java
+++ Project 2/src/edu/ncsu/csc216/stp/model/io/TestPlanReader.java
@@ -2,6 +2,12 @@
  * 
  */
 package edu.ncsu.csc216.stp.model.io;
+import java.io.File;
+
+import edu.ncsu.csc216.stp.model.test_plans.AbstractTestPlan;
+import edu.ncsu.csc216.stp.model.test_plans.TestPlan;
+import edu.ncsu.csc216.stp.model.tests.TestCase;
+import edu.ncsu.csc216.stp.model.util.ISortedList;
 
 /**
  * @author mcphe
@@ -9,4 +15,19 @@ package edu.ncsu.csc216.stp.model.io;
  */
 public class TestPlanReader {
 
+	
+	public static ISortedList<TestPlan> readTestPlansFile(File file) {
+		
+		return null;
+	}
+	
+	private TestPlan processTestPlan(String testPlan) {
+		
+		return null;
+	}
+	
+	private TestCase processTest(AbstractTestPlan plan, String caseString) {
+		
+		return null;
+	}
 }
