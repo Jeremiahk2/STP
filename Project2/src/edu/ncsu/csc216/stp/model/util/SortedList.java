7af3d6ca91cc9e1604a7236116c9995aacfc00ed
Jeremiah Knizley <jdknizle@ncsu.edu>
2022-11-06 01:00:14 -0400

    Created skeleton of SwapList. Implemented interface in SortedList,
    Deleted ListNode.java and remade it as an inner class of SortedList.
    Added skeleton of overridden methods in SortedList and implemented
    fields.

--- Project2/src/edu/ncsu/csc216/stp/model/util/SortedList.java
+++ Project2/src/edu/ncsu/csc216/stp/model/util/SortedList.java
@@ -4,9 +4,58 @@
 package edu.ncsu.csc216.stp.model.util;
 
 /**
- * @author mcphe
+ * 
+ * @author Jeremiah Knizley
  *
+ * @param <E>
  */
-public class SortedList<E> {
+public class SortedList<E extends Comparable<E>> implements ISortedList<E> {
+
+	private ListNode front;
+	
+	public SortedList() {
+		
+	}
+	
+	@Override
+	public void add(E element) {
+		
+	}
+
+	@Override
+	public E remove(int idx) {
+		return null;
+	}
+
+	@Override
+	public boolean contains(E element) {
+		return false;
+	}
+
+	@Override
+	public E get(int idx) {
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+	/**
+	 * @author Jeremiah Knizley
+	 *
+	 */
+	public class ListNode {
+
+		public ListNode next;
+		
+		public E data;
+		
+		public ListNode(E element, ListNode next) {
+			
+		}
+		
+	}
+
 
 }
--- Project2/src/edu/ncsu/csc216/stp/model/util/SwapList.java
+++ Project2/src/edu/ncsu/csc216/stp/model/util/SwapList.java
@@ -4,9 +4,62 @@
 package edu.ncsu.csc216.stp.model.util;
 
 /**
- * @author mcphe
+ * @author Jeremiah Knizley
  *
  */
-public class SwapList<E> {
+public class SwapList<E> implements ISwapList<E> {
+	
+	private static final int INITIAL_CAPACITY = 10;
+	
+	private E[] list;
+	
+	private int size;
+	
+	public SwapList() {
+		
+	}
+	
+	@Override
+	public void add(E element) {
+		
+	}
+
+	@Override
+	public E remove(int idx) {
+		
+		return null;
+	}
+
+	@Override
+	public void moveUp(int idx) {
+		
+		
+	}
+
+	@Override
+	public void moveDown(int idx) {
+		
+		
+	}
+
+	@Override
+	public void moveToFront(int idx) {
+		
+	}
+
+	@Override
+	public void moveToBack(int idx) {
+		
+	}
+
+	@Override
+	public E get(int idx) {
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
 
 }
