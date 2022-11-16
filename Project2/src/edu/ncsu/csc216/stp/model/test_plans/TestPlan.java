5774bef342039c4f0be4238e2873087f0e3c344b
Jeremiah Knizley <jdknizle@ncsu.edu>
2022-11-16 18:35:19 -0500

    [Test][Impelementation] Changed FAILING_TEST_LIST_NAME check in TestPlan
    constructor to check for the constant case-insensitively.

--- Project2/src/edu/ncsu/csc216/stp/model/test_plans/TestPlan.java
+++ Project2/src/edu/ncsu/csc216/stp/model/test_plans/TestPlan.java
@@ -21,7 +21,7 @@ public class TestPlan extends AbstractTestPlan implements Comparable<TestPlan> {
 	 */
 	public TestPlan(String testPlanName) {
 		super(testPlanName);
-		if (testPlanName.equals(FailingTestList.FAILING_TEST_LIST_NAME)) {
+		if (testPlanName.equalsIgnoreCase(FailingTestList.FAILING_TEST_LIST_NAME)) {
 			throw new IllegalArgumentException("Invalid name.");
 		}
 	}
